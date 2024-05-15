32626d49914b393a00e2a6deded0cad6ffa70326
iSTeeWx_ <dev.isteewx@gmail.com>
2024-05-16 00:43:22 +0200

    Added base 1.8.9 source with optifine

---
+++ src/test/java/Start.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.util.Arrays;
+
+import net.minecraft.client.main.Main;
+import org.apache.commons.lang3.ArrayUtils;
+
+public class Start {
+    public static void main(String[] args) {
+        System.setProperty("org.lwjgl.librarypath", new File("../workspace/versions/1.8.9/1.8.9-natives").getAbsolutePath());
+        Main.main(ArrayUtils.addAll(new String[]{"--version", "1.8.9", "--accessToken", "0", "--assetsDir", "assets", "--assetIndex", "1.8", "--userProperties", "{}"}, args));
+    }
+}
