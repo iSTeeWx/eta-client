ac614a5b46721135a41d7f7b1d841f4862b0e926
isteewx <dev.isteewx@gmail.com>
2024-10-06 10:01:55 +0200

    changed path to lwjgl

--- src/test/java/Start.java
+++ src/test/java/Start.java
@@ -6,7 +6,7 @@ import org.apache.commons.lang3.ArrayUtils;
 
 public class Start {
     public static void main(String[] args) {
-        System.setProperty("org.lwjgl.librarypath", new File("../workspace/versions/1.8.9/1.8.9-natives").getAbsolutePath());
+        System.setProperty("org.lwjgl.librarypath", new File("versions/1.8.9/1.8.9-natives").getAbsolutePath());
         Main.main(ArrayUtils.addAll(new String[]{"--version", "1.8.9", "--accessToken", "0", "--assetsDir", "assets", "--assetIndex", "1.8", "--userProperties", "{}"}, args));
     }
 }
